585084e9a870eceac64a1f0f7bb1211c922f7a28
brune <brune@brune.home>
2024-12-25 09:44:01 +0100

    create base classes

---
+++ src/main/java/com/architektura/architektura_procesor/GeneralRegisters.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.architektura.architektura_procesor;
+
+/**
+ *
+ * @author brune
+ */
+public class GeneralRegisters {
+    
+    private Short Rd;
+    private Short Rn;
+
+    public GeneralRegisters() {
+    }
+    
+    
+
+    public Short getRd() {
+        return Rd;
+    }
+
+    public void setRd(Short Rd) {
+        this.Rd = Rd;
+    }
+
+    public Short getRn() {
+        return Rn;
+    }
+
+    public void setRn(Short Rn) {
+        this.Rn = Rn;
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/architektura/architektura_procesor/Memory.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.architektura.architektura_procesor;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author brune
+ */
+public class Memory {
+    
+    private ArrayList<Short> memory = new ArrayList();
+
+    public Memory() {
+    }
+
+    public ArrayList<Short> getMemory() {
+        return memory;
+    }
+
+    public void setMemory(ArrayList<Short> memory) {
+        this.memory = memory;
+    }
+    
+    
+    
+}
