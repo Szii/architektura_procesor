8a553f23396b4c2531c775cf41e17dd65595c610
brune <brune@brune.home>
2024-12-25 09:23:44 +0100

    create basic project structure

---
+++ src/main/java/com/architektura/architektura_procesor/Architektura_procesor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.architektura.architektura_procesor;
+
+/**
+ *
+ * @author brune
+ */
+public class Architektura_procesor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
